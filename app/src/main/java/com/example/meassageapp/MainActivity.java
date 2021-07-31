c30a1b4e172eb77c38f091d72686b372fa1fbe03
Sovann Rath <rathsovann7@gmail.com>
2021-07-31 15:14:07 +0700

    Hello

--- app/src/main/java/com/example/meassageapp/MainActivity.java
+++ app/src/main/java/com/example/meassageapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.meassageapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText txtMoney;
+Button btnExchange;
+Button btnClose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtMoney=(EditText )findViewById(R.id.txtMoney ) ;
+        btnClose =(Button )findViewById(R.id.btnClose ) ;
+        btnClose .setOnClickListener((v) -> {
+        finish();
+        System.exit( 0);
+        });
+        btnExchange =(Button )findViewById(R.id.btnExchange );
+
     }
 }
